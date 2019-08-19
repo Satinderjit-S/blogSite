bdafb4cf5c63c329b01df1df9a35e0cc02f6d600
unknown <saisatinderjitsingh@gmail.com>
2019-08-19 19:37:25 -0400

    Ready for deployment on heroku

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require("lodash");
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://Satinderjit:Test123@cluster0-u9dkq.mongodb.net/Posts",{useNewUrlParser:true}); // db creation
+const postSchema=new mongoose.Schema({
+  title:String,
+  content:String
+}); //schema creation
+const Post=new mongoose.model("Post",postSchema);
+
+
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -11,7 +22,6 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts=[];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,7 +33,10 @@ app.use(express.static("public"));
 
 
 app.get('/',function(req,res){
+  Post.find({},function(err,posts){
   res.render('home',{hSContent:homeStartingContent,posts:posts});
+  });
+
 });
 app.get('/about',function(req,res){
   res.render('about',{aboutContent:aboutContent});
@@ -35,24 +48,28 @@ app.get('/compose',function(req,res){
   res.render('compose');
 });
 app.post('/compose',function(req,res){
-  const post = {
+  const post=new Post({
     title:req.body.postTitle,
-    body:req.body.postContent
-  };
-  posts.push(post);
-  res.redirect('/');
+    content:req.body.postContent
+  });
+  post.save(function(err){
+      if(!err){
+        res.redirect('/');
+      }
+  });
+
 
 });
 
 
-app.get("/posts/:postTitle",function(req,res){
-  posts.forEach(function(post){
-      if((_.lowerCase(post.title))===_.lowerCase(req.params.postTitle))
-      {
-        res.render("post",{postTitle:post.title,postBody:post.body});
-      }
+app.get("/posts/:postId",function(req,res){
+  const reqPostId=req.params.postId;
 
+  Post.findOne({_id:reqPostId},function(err,post){
+    res.render("post",{postTitle:post.title,postBody:post.content});
   });
+
+
 
 });
 
